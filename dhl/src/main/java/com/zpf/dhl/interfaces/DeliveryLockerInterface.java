1aabd868864d08a498b5eb419866201f9e2dc087
sh01060 <pengfei.zhou@shenmajr.com>
2018-11-09 13:05:27 +0800

    进度保存

--- dhl/src/main/java/com/zpf/dhl/interfaces/DeliveryLockerInterface.java
+++ dhl/src/main/java/com/zpf/dhl/interfaces/DeliveryLockerInterface.java
@@ -3,7 +3,6 @@ package com.zpf.dhl.interfaces;
 /**
  * Created by ZPF on 2018/11/9.
  */
-
 public interface DeliveryLockerInterface {
     void put(ExpressageInterface expressageInterface);
 
--- dhl/src/main/java/com/zpf/dhl/interfaces/ParcelReceiverInterface.java
+++ dhl/src/main/java/com/zpf/dhl/interfaces/ParcelReceiverInterface.java
@@ -4,7 +4,7 @@ package com.zpf.dhl.interfaces;
  * 收件人
  * Created by ZPF on 2018/11/8.
  */
-public interface ParcelReceiverInterface extends ParcelDisposeInterface,ParcelInspectorInterface {
+public interface ParcelReceiverInterface extends ParcelDisposeInterface, ParcelInspectorInterface {
 
     //接收人
     String getReceiver();
